d5c4ffac73e64d86210cc0dd0a6f63c823286456
Daniil Pavlov <hgh2005dan@gmail.com>
2021-12-18 22:18:46 +0300

    минорные изменения

--- 1.py
+++ 1.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import sys
 from time import sleep
 
 import pygame
@@ -8,15 +10,35 @@ pygame.display.set_caption('pygame-project')
 # screen = pygame.display.set_mode((1280, 720),
 # pygame.FULLSCREEN) с fullscreen не удобно при написании, потом поставим
 
-screen = pygame.display.set_mode((1280, 720))
+SIZE = WIDTH, HEIGHT = 1280, 720
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 MENU_ACTIVE_BTN_COLOR = pygame.Color('#40009A')
 MENU_INACTIVE_BTN_COLOR = pygame.Color('#8130F4')
 MENU_BTN_SOUND = pygame.mixer.Sound('sounds/menu_btn.wav')
 
 
+def load_image(name, colorkey=None):
+    fullname = os.path.join('images', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
 class Button:
-    """ Класс кнопок для создания непосредственно самих конпок """
+    """
+    Класс кнопок для создания непосредственно самих конпок
+    """
 
     def __init__(self, width, height):
         self.width = width
@@ -24,30 +46,34 @@ class Button:
         self.inactive_color = MENU_INACTIVE_BTN_COLOR
         self.active_color = MENU_ACTIVE_BTN_COLOR
 
-    def draw(self, x, y, message, action=None, font_size=50):
+    def draw(self, x, y, message, button_image, action=None, font_size=50):
         mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
         clicked = pygame.mouse.get_pressed()
         if (x < mouse_pos_x < x + self.width) and (y < mouse_pos_y < y + self.height):
-            pygame.draw.rect(screen, self.active_color, (x, y, self.width, self.height))
+            screen.blit(pygame.transform.scale(load_image(button_image), (self.width, self.height)), (x, y - 5))
+            font = pygame.font.Font(None, font_size)
+            text = font.render(message, True, 'white')
+            screen.blit(text, (x + ((self.width - text.get_width()) // 2), y + ((self.height - text.get_height()) // 2) - 5))
             if clicked[0]:
                 pygame.mixer.Sound.play(MENU_BTN_SOUND)
                 pygame.time.delay(300)
                 if action is not None:
                     action()
         else:
-            pygame.draw.rect(screen, self.inactive_color, (x, y, self.width, self.height))
-        # вот это надо буедт попраавить ибо не отрисовывает в нужном месте текст
-        font = pygame.font.Font(None, font_size)
-        text = font.render(message, True, 'white')
-
-        screen.blit(text, (x + text.get_width() // 2 - 20, y + text.get_height() // 2))
+            screen.blit(pygame.transform.scale(load_image(button_image), (self.width, self.height)), (x, y))
+            font = pygame.font.Font(None, font_size)
+            text = font.render(message, True, 'white')
+            screen.blit(text, (x + ((self.width - text.get_width()) // 2), y + ((self.height - text.get_height()) // 2)))
 
 
 def show_menu(screen, clock):
     """ Отрисовка самого меню """
-    menu_background = pygame.image.load('images/background.jpg')
+    menu_background = load_image('background.jpg')
     show = True
     start_btn = Button(300, 70)
+    settings_btn = Button(300, 70)
+    scores_btn = Button(300, 70)
+    exit_btn = Button(300, 70)
     while show:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -55,7 +81,10 @@ def show_menu(screen, clock):
                 quit()
 
         screen.blit(menu_background, (0, 0))
-        start_btn.draw(300, 200, 'Start game', game_cycle)
+        start_btn.draw(WIDTH - 500, HEIGHT - 500, 'Start game', 'btn_menu.png', game_cycle)
+        settings_btn.draw(WIDTH - 500, HEIGHT - 410, 'Settings', 'btn_menu.png', settings)
+        scores_btn.draw(WIDTH - 500, HEIGHT - 320, 'Scores', 'btn_menu.png', scores)
+        exit_btn.draw(WIDTH - 500, HEIGHT - 230, 'Exit', 'btn_menu.png', terminate)
         pygame.display.update()
         clock.tick(60)
 
@@ -75,7 +104,7 @@ def start_menu():
 
 def game_cycle():
     """ Отрисовка самого меню """
-    menu_background = pygame.image.load('images/background_2.jpg')
+    menu_background = load_image('background_2.jpg')
     show = True
     while show:
         for event in pygame.event.get():
@@ -85,12 +114,52 @@ def game_cycle():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-               #     logging.warning('game exit by keys')
+                    #     logging.warning('game exit by keys')
                     show = False
 
         screen.blit(menu_background, (0, 0))
         pygame.display.update()
 
+
+def settings():
+    menu_background = load_image('background_2.jpg')
+    show = True
+    while show:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    #     logging.warning('game exit by keys')
+                    show = False
+
+        screen.blit(menu_background, (0, 0))
+        pygame.display.update()
+
+
+def scores():
+    menu_background = load_image('background_2.jpg')
+    show = True
+    while show:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    #     logging.warning('game exit by keys')
+                    show = False
+
+        screen.blit(menu_background, (0, 0))
+        pygame.display.update()
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
 
 
 if __name__ == '__main__':
