5757d9191c16a98e0d95be1e0a08ac08596d90b0
Daniil Pavlov <hgh2005dan@gmail.com>
2021-12-19 17:15:23 +0300

    menu

--- 1.py
+++ 1.py
@@ -13,8 +13,11 @@ pygame.display.set_caption('pygame-project')
 SIZE = WIDTH, HEIGHT = 1280, 720
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-MENU_ACTIVE_BTN_COLOR = pygame.Color('#40009A')
-MENU_INACTIVE_BTN_COLOR = pygame.Color('#8130F4')
+volume = 60
+change_difficult = 0
+select_lang = 0
+DIFFICULTY = ['easy', 'normal', 'hard', 'cheat']
+LANGUAGES = ['en', 'ru']
 MENU_BTN_SOUND = pygame.mixer.Sound('sounds/menu_btn.wav')
 
 
@@ -43,17 +46,16 @@ class Button:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.inactive_color = MENU_INACTIVE_BTN_COLOR
-        self.active_color = MENU_ACTIVE_BTN_COLOR
 
-    def draw(self, x, y, message, button_image, action=None, font_size=50):
+    def draw(self, x, y, button_image, message=None, action=None, font_size=50):
         mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
         clicked = pygame.mouse.get_pressed()
         if (x < mouse_pos_x < x + self.width) and (y < mouse_pos_y < y + self.height):
             screen.blit(pygame.transform.scale(load_image(button_image), (self.width, self.height)), (x, y - 5))
             font = pygame.font.Font(None, font_size)
             text = font.render(message, True, 'white')
-            screen.blit(text, (x + ((self.width - text.get_width()) // 2), y + ((self.height - text.get_height()) // 2) - 5))
+            screen.blit(text,
+                        (x + ((self.width - text.get_width()) // 2), y + ((self.height - text.get_height()) // 2) - 5))
             if clicked[0]:
                 pygame.mixer.Sound.play(MENU_BTN_SOUND)
                 pygame.time.delay(300)
@@ -63,7 +65,21 @@ class Button:
             screen.blit(pygame.transform.scale(load_image(button_image), (self.width, self.height)), (x, y))
             font = pygame.font.Font(None, font_size)
             text = font.render(message, True, 'white')
-            screen.blit(text, (x + ((self.width - text.get_width()) // 2), y + ((self.height - text.get_height()) // 2)))
+            screen.blit(text,
+                        (x + ((self.width - text.get_width()) // 2), y + ((self.height - text.get_height()) // 2)))
+
+
+class SystemButton:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def draw(self, x, y, image, message, font_size=50):
+        screen.blit(pygame.transform.scale(load_image(image), (self.width, self.height)), (x, y))
+        font = pygame.font.Font(None, font_size)
+        text = font.render(message, True, 'white')
+        screen.blit(text,
+                    (x + ((self.width - text.get_width()) // 2), y + ((self.height - text.get_height()) // 2)))
 
 
 def show_menu(screen, clock):
@@ -81,10 +97,10 @@ def show_menu(screen, clock):
                 quit()
 
         screen.blit(menu_background, (0, 0))
-        start_btn.draw(WIDTH - 500, HEIGHT - 500, 'Start game', 'btn_menu.png', game_cycle)
-        settings_btn.draw(WIDTH - 500, HEIGHT - 410, 'Settings', 'btn_menu.png', settings)
-        scores_btn.draw(WIDTH - 500, HEIGHT - 320, 'Scores', 'btn_menu.png', scores)
-        exit_btn.draw(WIDTH - 500, HEIGHT - 230, 'Exit', 'btn_menu.png', terminate)
+        start_btn.draw(WIDTH - 500, HEIGHT - 500, 'btn_menu.png', 'Start game', game_cycle)
+        settings_btn.draw(WIDTH - 500, HEIGHT - 410, 'btn_menu.png', 'Settings', settings)
+        scores_btn.draw(WIDTH - 500, HEIGHT - 320, 'btn_menu.png', 'Scores', scores)
+        exit_btn.draw(WIDTH - 500, HEIGHT - 230, 'btn_menu.png', 'Exit', terminate)
         pygame.display.update()
         clock.tick(60)
 
@@ -122,7 +138,16 @@ def game_cycle():
 
 
 def settings():
-    menu_background = load_image('background_2.jpg')
+    settings_background = load_image('background_2.jpg')
+    volume_btn = SystemButton(400, 135)
+    difficulty_btn = SystemButton(400, 135)
+    language_btn = SystemButton(400, 135)
+    change_lang_btn = Button(40, 40)
+    quiet_btn = Button(40, 15)
+    loud_btn = Button(40, 40)
+    easier_btn = Button(40, 15)
+    harder_btn = Button(40, 40)
+    about_btn = Button(400, 135)
     show = True
     while show:
         for event in pygame.event.get():
@@ -132,10 +157,19 @@ def settings():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    #     logging.warning('game exit by keys')
                     show = False
+        screen.blit(settings_background, (0, 0))
+        volume_btn.draw(460, 50, 'settings_btns.png', f'Volume {volume}%')
+        difficulty_btn.draw(460, 200, 'settings_btns.png', f"Difficulty: {DIFFICULTY[change_difficult]}", 40)
+        language_btn.draw(460, 350, 'settings_btns.png', f'Language: {LANGUAGES[select_lang]}')
+        about_btn.draw(460, 500, 'settings_btns.png', 'About', about_widget)
 
-        screen.blit(menu_background, (0, 0))
+        quiet_btn.draw(500, 110, 'minus_btn.png', '', quiet_volume)
+        loud_btn.draw(780, 95, 'plus_btn.png', '', loud_volume)
+        easier_btn.draw(500, 260, 'minus_btn.png', '', reduce_difficult)
+        harder_btn.draw(780, 245, 'plus_btn.png', '', increase_difficult)
+        change_lang_btn.draw(780, 395, 'plus_btn.png', '', change_language)
+
         pygame.display.update()
 
 
@@ -154,6 +188,70 @@ def scores():
                     show = False
 
         screen.blit(menu_background, (0, 0))
+        pygame.display.update()
+
+
+def loud_volume():
+    global volume
+    volume_btn = SystemButton(400, 135)
+    volume += 10
+    volume_btn.draw(460, 200, 'settings_btns.png', f'Volume {volume}%')
+    if volume == 110:
+        volume = 0
+
+
+def quiet_volume():
+    global volume
+    volume_btn = SystemButton(400, 135)
+    volume -= 10
+    volume_btn.draw(460, 200, 'settings_btns.png', f'Volume {volume}%')
+    if volume == -10:
+        volume = 100
+
+
+def increase_difficult():
+    global change_difficult
+    difficulty_btn = SystemButton(400, 135)
+    change_difficult += 1
+    if change_difficult == 4:
+        change_difficult = 0
+        difficulty_btn.draw(460, 355, 'settings_btns.png', f"Difficulty: {DIFFICULTY[change_difficult]}")
+    difficulty_btn.draw(460, 355, 'settings_btns.png', f"Difficulty: {DIFFICULTY[change_difficult]}")
+
+
+def reduce_difficult():
+    global change_difficult
+    difficulty_btn = SystemButton(400, 135)
+    change_difficult -= 1
+    difficulty_btn.draw(460, 355, 'settings_btns.png', f"Difficulty: {DIFFICULTY[change_difficult]}")
+    if change_difficult == -1:
+        change_difficult = 3
+
+
+def change_language():
+    global select_lang
+    language_btn = SystemButton(400, 135)
+    select_lang += 1
+    if select_lang == 2:
+        select_lang = 0
+        language_btn.draw(460, 510, 'settings_btns.png', f'Language: {LANGUAGES[select_lang % 2]}')
+    language_btn.draw(460, 510, 'settings_btns.png', f'Language: {LANGUAGES[select_lang % 2]}')
+
+
+def about_widget():
+    about = pygame.transform.scale(load_image('about.png'), (700, 600))
+    show = True
+    while show:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    show = False
+
+        screen.blit(about, (300, 40))
         pygame.display.update()
 
 
